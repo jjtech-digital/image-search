3e7cd9d5b57240d239d79d25b2536dbecdb0a8a9
chanchal013 <chanchal@compose.co.in>
2024-05-20 11:07:01 +0530

    feat: add switch camera function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,34 @@ import { IconCamera, IconCameraOff } from "@tabler/icons-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const FACING_MODE_USER = "user";
+const FACING_MODE_ENVIRONMENT = "environment";
+
 export default function Home() {
   const webcamRef = useRef<Webcam>(null);
   const [isCameraActive, setIsCameraActive] = useState<boolean>(false);
   const [imgSrc, setImgSrc] = useState<any>(null);
+  const [facingMode, setFacingMode] = useState<string>(FACING_MODE_USER);
 
-  const capture = useCallback(() => {
+  const onCapture = useCallback(() => {
     const imageSrc = webcamRef?.current?.getScreenshot();
     setImgSrc(imageSrc);
   }, [webcamRef]);
+
+  let videoConstraints: MediaTrackConstraints = {
+    facingMode: facingMode,
+    width: 500,
+    height: 480,
+  };
+
+  const toggleCamera = useCallback(() => {
+    setFacingMode((prevState) =>
+      prevState === FACING_MODE_USER
+        ? FACING_MODE_ENVIRONMENT
+        : FACING_MODE_USER
+    );
+  }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-start gap-6 p-20 ${inter.className}`}
@@ -40,24 +59,27 @@ export default function Home() {
       <div>
         {imgSrc && <Image src={imgSrc} width={500} height={500} alt="webcam" />}
         {isCameraActive && !imgSrc && (
-          <Webcam height={500} width={500} ref={webcamRef} />
+          <Webcam
+            height={500}
+            width={500}
+            ref={webcamRef}
+            mirrored
+            videoConstraints={videoConstraints}
+          />
         )}
-        <div className="p-4">
-          {imgSrc ? (
-            <button
-              className="p-3 border-2 border-gray-400 hover:bg-slate-400"
-              onClick={() => setImgSrc(null)}
-            >
-              Retake photo
-            </button>
-          ) : (
-            <button
-              className="p-3 border-2 border-gray-400 hover:bg-slate-400"
-              onClick={capture}
-            >
-              Capture photo
-            </button>
-          )}
+        <div className="p-4 flex gap-3">
+          <button
+            className="p-3 border-2 border-gray-400 hover:bg-slate-400"
+            onClick={imgSrc ? () => setImgSrc(null) : onCapture}
+          >
+            {imgSrc ? "Retake photo" : "Capture photo"}
+          </button>
+          <button
+            className="p-3 border-2 border-gray-400 hover:bg-slate-400"
+            onClick={toggleCamera}
+          >
+            Switch camera
+          </button>
         </div>
       </div>
     </main>
